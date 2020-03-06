c69195680c9dcc531686a602bc5f3700c76f4f8d
michael00987 <hyeongkwangseok@gmail.com>
2020-03-06 13:16:24 +0900

    sutdy hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+// import { render } from '@testing-library/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>hello world</h1>
+      <FuncComp initNumber={2} />
+      <ClassComp initNumber={2} />
     </div>
   );
+}
+/***********/
+let funcId = 0;
+function FuncComp(props) {
+  const numberState = useState(props.initNumber);
+  const number = numberState[0];
+  const setNumber = numberState[1];
+
+  const [_date, setDate] = useState(new Date().toString());
+  // const dateState = useState(new Date().toString());
+  // const _date = dateState[0];
+  // const setDate = dateState[1];
+
+  useEffect(() => {
+    console.log(
+      '%cfunc => useEffect' + '(componentDidMount ) A' + ++funcId,
+      'color:white'
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      '%cfunc => useEffect number' +
+        '(componentDidMount&componentDidUpdate )B' +
+        ++funcId,
+      'color:white'
+    );
+    document.title = number;
+    return function cleanUp() {
+      console.log(
+        '%cfunc => useEffect return' +
+          'number' +
+          '(componentWillUnmount)' +
+          ++funcId,
+        'color:white'
+      );
+    };
+  }, [number]);
+
+  console.log('%cfunc => render' + ++funcId, 'color:blue');
+  return (
+    <div className="container">
+      <h1>function style</h1>
+      <p>Number: {number}</p>
+      <p>Date: {_date}</p>
+      <input
+        type="submit"
+        value="random"
+        onClick={() => {
+          setNumber(Math.random());
+        }}
+      />
+      <input
+        type="submit"
+        value="date"
+        onClick={() => {
+          setDate(new Date().toString());
+        }}
+      />
+    </div>
+  );
+}
+
+/*******/
+class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber,
+    date: new Date().toString()
+  };
+
+  componentDidMount() {
+    console.log('%cclass => componentDidMount', 'color:red');
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('%cclass => shouldComponentUpdate', 'color:red');
+    return true;
+  }
+  componentDidUpdate(nextProps, nextState) {
+    console.log('%cclass => componentDidUpdate', 'color:red');
+  }
+  render() {
+    console.log('%cclass => render', 'color:red');
+    const { number, date } = this.state;
+    return (
+      <div className="container">
+        <h1>class style</h1>
+        <p>Number: {number}</p>
+        <p>Date: {date}</p>
+        <input
+          type="submit"
+          value="random"
+          onClick={() => {
+            this.setState({ number: Math.random() });
+          }}
+        />
+        <input
+          type="submit"
+          value="date"
+          onClick={() => {
+            this.setState({ date: new Date().toString() });
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
